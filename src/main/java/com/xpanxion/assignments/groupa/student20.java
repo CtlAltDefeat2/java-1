644ce22e78b853b4059f10a8b3f8d52f1e76e68c
Elden-Lord01 <levi.melton43@gmail.com>
2022-04-25 14:18:31 -0400

    exc1

--- src/main/java/com/xpanxion/assignments/groupa/student20.java
+++ src/main/java/com/xpanxion/assignments/groupa/student20.java
@@ -1,13 +1,21 @@
 package com.xpanxion.assignments.groupa;
 
+import java.util.ArrayList;
+
 public class student20 {
     public static void main(String[] args) {
         ex1();
     }
 
     private static void ex1() {
-        System.out.println("Ex. 1");
+
+        int[] ints = {1, 2, 3};
+
+        var r = addNumbers( null);
+        System.out.println(r);
+
     }
+
 
     private static void ex2() {
         System.out.println("Ex. 2");
@@ -15,5 +23,10 @@ public class student20 {
 
     private static void ex3() {
         System.out.println("Ex. 3");
+    }
+
+    private static int addNumbers(ArrayList<Integer> numList) {
+        // TODO: Implement this function.
+        return 0;
     }
 }
