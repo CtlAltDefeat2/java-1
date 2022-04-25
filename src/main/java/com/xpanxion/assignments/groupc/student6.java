6cfa8d96faa0cf5df80cb591cebf82910a4c8f5d
daltonhutt <103676585+daltonhutt@users.noreply.github.com>
2022-04-25 17:43:48 -0400

    Class exercise 1 & 2.

--- src/main/java/com/xpanxion/assignments/groupc/student6.java
+++ src/main/java/com/xpanxion/assignments/groupc/student6.java
@@ -1,31 +1,69 @@
 package com.xpanxion.assignments.groupc;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class student6 {
     public static void main(String[] args) {
         ex1();
+        ex2();
+        ex3();
     }
 
     private static void ex1() {
         int[] ints = {1, 2, 3};
-        // But conversion here...
-
-        var r = addNumbers(null); // how to convert int[] to an ArrayList<Integer>.
-        System.out.println(r);
+        var intList = new ArrayList<Integer>(ints.length);
+        int sum = 0;
+        for (int i : ints)
+        {
+            intList.add(i);
+        }
+        System.out.println(addNumbers(intList));
     }
-
     private static void ex2() {
-        System.out.println("Ex. 2");
+        int[] ints = {1, 2, 3};
+        var intList = new ArrayList<Integer>(ints.length);
+        int sum  = 0;
+        for (int i : ints)
+        {
+            intList.add(i);
+        }
+        System.out.println(avgNumbers(intList));
     }
 
     private static void ex3() {
-        System.out.println("Ex. 3");
+        int[] ints = {1, 2, 3};
+        var intList = new ArrayList<Double>(ints.length);
+        int sum = 0;
+        for (int i : ints)
+        {
+            intList.add(i);
+        }
+        System.out.println(double(intList));
     }
 
-    private static Integer addNumbers(ArrayList<Integer> numList) {
-        // TODO: Implement this function.
-        return 0;
+    private static void doubleNum(ArrayList<Integer> numList) {
+        int[] ints = {1, 2, 3};
+        ArrayList<Integer> doubleArray = ints.stream().map
+
+
+
+
+
+
     }
 
+    private static Float avgNumbers(ArrayList<Integer> numList) {
+        float sum = 0, avg;
+        for (int i=0; i < numList.size(); i++)
+            sum += numList.get(i);
+        avg = (float) sum / numList.size();
+        return avg;
+    }
+    private static int addNumbers(ArrayList<Integer> numList) {
+        int sum = 0;
+        for (int i=0; i < numList.size(); i++)
+            sum += numList.get(i);
+        return sum;
+    }
 }
