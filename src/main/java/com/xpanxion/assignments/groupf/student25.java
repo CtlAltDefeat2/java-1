393fba7c348a2cb8a38c10f16a27f2aa968f199f
wdelacruzl <wdelacruzl98@gmail.com>
2022-04-25 16:49:31 -0400

    Ex 1

--- src/main/java/com/xpanxion/assignments/groupf/student25.java
+++ src/main/java/com/xpanxion/assignments/groupf/student25.java
@@ -18,17 +18,22 @@ public class student25 {
         System.out.println("The sum is " + sum);
     }
         private static Integer addNumbers(ArrayList<Integer> numList) {
-            int[] ints = {1,2,3};
             int sum = 0;
-            for(int i = 0; i < ints.length; i++){
-                sum = sum + ints[i];
+            for(int i = 0; i < numList.size(); i++){
+                sum += numList.get(i);
             }
             return sum;
 
         }
 
     private static void ex2() {
-        System.out.println("Ex. 2");
+        int[] ints = {1, 2, 3};
+
+
+
+
+
+            System.out.println("Ex. 2");
     }
 
     private static void ex3() {
