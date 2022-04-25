6f727d56964d6eef2636ed9ea4d4b06a58657558
raji <rmallina@xpanxion.com>
2022-04-25 12:32:30 -0400

    Raji Mallina

--- src/main/java/com/xpanxion/assignments/groupd/student19.java
+++ src/main/java/com/xpanxion/assignments/groupd/student19.java
@@ -2,14 +2,28 @@ package com.xpanxion.assignments.groupd;
 
 public class student19 {
     public static void main(String[] args) {
+        ex1();
+        ex2();
         System.out.println("Hello My name is Raji Mallina");
     }
 
     private static void ex1() {
+        String str = "Raji Mallina";
+        //Converting above input string to
+        //Uppercase letters to UpperCase method
+        String strup = str.toUpperCase();
+        //print uppercased string
+        System.out.println(strup);
+
+    } {
         System.out.println("Ex. 1");
-    }
+}
 
     private static void ex2() {
+
+
+
+
         System.out.println("Ex. 2");
     }
 
