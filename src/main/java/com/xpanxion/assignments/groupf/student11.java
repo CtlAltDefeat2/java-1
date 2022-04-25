a739ef9b3eed9e6c019fa7cb9347b19fbb349c16
IsabelGrandison <103618679+IsabelGrandison@users.noreply.github.com>
2022-04-25 16:52:00 -0400

    Assignment One

--- src/main/java/com/xpanxion/assignments/groupf/student11.java
+++ src/main/java/com/xpanxion/assignments/groupf/student11.java
@@ -1,13 +1,34 @@
 package com.xpanxion.assignments.groupf;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class student11 {
     public static void main(String[] args) {
-        ex1();
+    ex1();
     }
-
     private static void ex1() {
-        System.out.println("I am IG");
+    int[] ints = {1, 2, 3};
+    var numList = new ArrayList<Integer>(ints.length);
+    for(int i : ints) {
+        numList.add(i);
     }
+    var sum = addNumbers(numList);
+        System.out.println("The sum is" + sum);
+    }
+private static Integer addNumbers(ArrayList<Integer> numList) {
+    int sum = 0;
+    for (int i = 0; i < numList.size(); i++) {
+        sum +=numList.get(i);
+    }
+    return sum;
+}
+
+
+
+
+
+
 
     private static void ex2() {
         System.out.println("Ex. 2");
@@ -16,4 +37,6 @@ public class student11 {
     private static void ex3() {
         System.out.println("Ex. 3");
     }
-}
+
+
+    }
