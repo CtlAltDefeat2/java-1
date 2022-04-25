901600269895b67562d817b7d42ebcff56068058
dragoliajr <103143873+dragoliajr@users.noreply.github.com>
2022-04-25 14:04:50 -0400

    intro ex 1

--- src/main/java/com/xpanxion/assignments/groupd/student7.java
+++ src/main/java/com/xpanxion/assignments/groupd/student7.java
@@ -1,21 +1,29 @@
 package com.xpanxion.assignments.groupd;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class student7 {
     public static void main(String[] args) {
+        int[] ints = {1, 2, 3};
+        ArrayList<Integer> num = new ArrayList<Integer>() ;
+        for (int i = 0; i < ints.length; i++) {
+            num.add(ints[i]);
+        }
+        System.out.println(addNumbers(num));
 
-        ex1();
     }
 
-    private static void ex1() { //turn a person name into all uppercase
-        System.out.println("Ex. 1");
-        System.out.print("Enter Name: ");
-        Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
-        System.out.println(name.toUpperCase());
+    private static Integer addNumbers(ArrayList<Integer> numList) {
+        int sum = 0;
+        for (int i = 0; i < numList.size(); i++) {
+            sum += numList.get(i);
+        }
+        // TODO: Implement this function.
 
-
+        return sum;
     }
 
     private static void ex2() {
