c27cd57281cfe4a4959e29c808655a2566467e56
IsabelGrandison <103618679+IsabelGrandison@users.noreply.github.com>
2022-04-25 14:02:51 -0400

    update

--- src/main/java/com/xpanxion/assignments/groupf/student11.java
+++ src/main/java/com/xpanxion/assignments/groupf/student11.java
@@ -6,7 +6,7 @@ public class student11 {
     }
 
     private static void ex1() {
-        System.out.println("Ex. 1");
+        System.out.println("I am IG");
     }
 
     private static void ex2() {
