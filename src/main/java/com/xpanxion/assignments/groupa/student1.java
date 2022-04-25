c96a707fc135e4d5e8117bbbac9275c2bc728254
akbagga1 <103646402+akbagga1@users.noreply.github.com>
2022-04-25 16:42:32 -0400

    50 percent done with ex1

--- src/main/java/com/xpanxion/assignments/groupa/student1.java
+++ src/main/java/com/xpanxion/assignments/groupa/student1.java
@@ -1,12 +1,42 @@
 package com.xpanxion.assignments.groupa;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 public class student1 {
+
     public static void main(String[] args) {
         ex1();
+        //ex2();
+        //ex3();
     }
-
     private static void ex1() {
-        System.out.println("Ex. 1");
+        int[] array = {1, 2, 3}; // n = 3, n-1 = 2
+
+        // Convert int[] array to ArrayList<Integer>
+
+
+
+        var list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        var r = addNumbers(list);
+        System.out.println(r);
+
+//        int sum = 0;
+//            /*for(int i=0; i<=array.length-1; i++);   //0 1 2  //a.length =3, a.length-1=2
+//            {
+//                sum=sum+array[i]; //1,2,3 ==6
+//            }*/
+//
+//        //Enhanced for loop
+//        for (int value : array) {
+//            sum = sum + value;   //0+1=1, 1+2=3, 3+3=6
+//        }
+//        System.out.println("Sum of Array:" + sum);
     }
 
     private static void ex2() {
@@ -16,5 +46,14 @@ public class student1 {
     private static void ex3() {
         System.out.println("Ex. 3");
     }
+
+    private static Integer addNumbers(ArrayList<Integer> numList) {
+        Integer retVal = 0;
+        for (Integer i : numList) { // foreach
+            retVal += i;
+        }
+        return retVal;
+    }
+
 }
 
