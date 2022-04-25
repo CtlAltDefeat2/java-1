efadbd5e7897d5f38989f1be8ec99c3f7e929dfd
katie <kcohen015@gmail.com>
2022-04-25 13:44:28 -0400

    update

--- src/main/java/com/xpanxion/assignments/groupf/student14.java
+++ src/main/java/com/xpanxion/assignments/groupf/student14.java
@@ -6,7 +6,7 @@ public class student14 {
     }
 
     private static void ex1() {
-        System.out.println("Ex. 1");
+        System.out.println("Katie");
     }
 
     private static void ex2() {
