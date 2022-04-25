f51f8e1727506a6e30a149f6de54a88970eb7ae2
sniang1 <96024696+sniang1@users.noreply.github.com>
2022-04-25 14:20:01 -0400

    EXEC1

--- src/main/java/com/xpanxion/assignments/groupe/student22.java
+++ src/main/java/com/xpanxion/assignments/groupe/student22.java
@@ -1,13 +1,39 @@
 package com.xpanxion.assignments.groupe;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class student22 {
     public static void main(String[] args) {
-        ex1();
+        addNumbers();
+        avgNumbers();
     }
 
+   private static void addNumbers(){
+        int[] ints = {1,2,3};
+       int sumOfInts = 0;
+        for(int i = 0; i < ints.length; i++) {
+             sumOfInts += ints[i];
+
+         }
+       System.out.println(sumOfInts);
+   }
+    private static void avgNumbers(){
+      int[] ints = {1,2,3};
+        int sumOfInts = 0;
+        float avgOfInts = 0;
+        for(int i = 0; i < ints.length; i++) {
+            sumOfInts += ints[i];
+            avgOfInts = sumOfInts / 2;
+
+        }
+        System.out.println(avgOfInts);
+
+    }
     private static void ex1() {
         System.out.println("Ex. 1");
     }
+
 
     private static void ex2() {
         System.out.println("Ex. 2");
@@ -16,4 +42,5 @@ public class student22 {
     private static void ex3() {
         System.out.println("Ex. 3");
     }
+
 }
