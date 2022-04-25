6c6a1499766ef3d147cc8bbb904a85769257536e
jbrennan7651 <103645942+jbrennan7651@users.noreply.github.com>
2022-04-25 12:15:29 -0400

    Example 1

--- src/main/java/com/xpanxion/assignments/groupc/student12.java
+++ src/main/java/com/xpanxion/assignments/groupc/student12.java
@@ -1,5 +1,7 @@
 package com.xpanxion.assignments.groupc;
 
+import java.util.ArrayList;
+
 public class student12 {
     public static void main(String[] args) {
         ex1();
@@ -7,6 +9,19 @@ public class student12 {
 
     private static void ex1() {
         System.out.println("Ex. 1");
+        addNumbers(new ArrayList<>());
+    }
+    private static Integer addNumbers(ArrayList<Integer> numList) {
+        int[] ints = {1, 2, 3};
+        int sum = 0;
+        var list1 = new ArrayList<Integer>();
+        for (int i = 0; i < ints.length; i++){
+            list1.add(ints[i]);
+            sum += list1.get(i);
+        }
+        System.out.println("Sum of all numbers in Array: " + sum);
+
+        return 0;
     }
 
     private static void ex2() {
@@ -16,4 +31,6 @@ public class student12 {
     private static void ex3() {
         System.out.println("Ex. 3");
     }
+
 }
+
