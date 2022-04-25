db73cb1484f891df44aa1bed0bf03308bb89029b
raji <rmallina@xpanxion.com>
2022-04-25 13:23:57 -0400

    Ex.2

--- src/main/java/com/xpanxion/assignments/groupd/student19.java
+++ src/main/java/com/xpanxion/assignments/groupd/student19.java
@@ -2,9 +2,9 @@ package com.xpanxion.assignments.groupd;
 
 public class student19 {
     public static void main(String[] args) {
-        ex1();
+        //ex1();
         ex2();
-        System.out.println("Hello My name is Raji Mallina");
+        //System.out.println("Hello My name is Raji Mallina");
     }
 
     private static void ex1() {
@@ -20,7 +20,18 @@ public class student19 {
 }
 
     private static void ex2() {
+        String str = "Raji1 Mallina";
+        int count = 0, upper = 0, lower = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch >= 'A' && ch <= 'Z')
+                upper++;
+            else if (ch >= 'a' && ch <= 'z')
+                lower++;
 
+        }
+        System.out.println("Lower case letters : " + lower);
+        System.out.println("Upper case letters : " + upper);
 
 
 
