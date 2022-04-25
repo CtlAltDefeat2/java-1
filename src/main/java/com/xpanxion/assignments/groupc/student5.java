d6c24e2da918c120aebe890b59de76aed25788a6
Connor Shillady <c.shillady24@gmail.com>
2022-04-25 15:20:49 -0400

    Exercise 2

--- src/main/java/com/xpanxion/assignments/groupc/student5.java
+++ src/main/java/com/xpanxion/assignments/groupc/student5.java
@@ -1,10 +1,12 @@
 package com.xpanxion.assignments.groupc;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class student5 {
     public static void main(String[] args) {
-        ex1();
+        //ex1();
+        ex2();
     }
     public static void ex1() {
         var array = new ArrayList<Integer>();
@@ -13,15 +15,27 @@ public class student5 {
     }
     public static Integer addNumbers(ArrayList<Integer> numList) {
         int[] ints = {1, 2, 3};
-        int sum = 0;
         List<Integer> array = Arrays.stream(ints).boxed().toList();
         int intSum = Arrays.stream(ints).sum();
         return intSum;
     }
     public static void ex2() {
+        var array2 = new ArrayList<Integer>();
         System.out.println("Ex. 2");
+        System.out.println(avgNumbers(array2));
     }
-
+    private static Float avgNumbers(ArrayList<Integer> avgArray) {
+        int[] ints = {1, 2, 3};
+        float avg = 0;
+        float sum = 0;
+        avgArray = new ArrayList<>();
+        for (int i = 0; i < ints.length; i++) {
+            avgArray.add(ints[i]);
+            sum += avgArray.get(i);
+        }
+        avg = sum / avgArray.size();
+        return avg;
+    }
     public static void ex3() {
         System.out.println("Ex. 3");
     }
