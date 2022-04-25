41b6c342651ddab2df03a5e8582da8669d03e140
Connor Shillady <c.shillady24@gmail.com>
2022-04-25 11:37:04 -0400

    Inital Commit

--- src/main/java/com/xpanxion/assignments/groupc/student5.java
+++ src/main/java/com/xpanxion/assignments/groupc/student5.java
@@ -2,18 +2,36 @@ package com.xpanxion.assignments.groupc;
 
 public class student5 {
     public static void main(String[] args) {
-        ex1();
+        ex1()
     }
-
-    private static void ex1() {
+    public static void ex1() {
         System.out.println("Ex. 1");
     }
-
-    private static void ex2() {
+    public static void ex2() {
         System.out.println("Ex. 2");
     }
-
-    private static void ex3() {
+    public static void ex3() {
         System.out.println("Ex. 3");
+    }
+    public static void ex4() {
+        System.out.println("Ex. 4");
+    }
+    public static void ex5() {
+        System.out.println("Ex. 5");
+    }
+    public static void ex6() {
+        System.out.println("Ex. 6");
+    }
+    public static void ex7() {
+        System.out.println("Ex. 7");
+    }
+    public static void ex8() {
+        System.out.println("Ex. 8");
+    }
+    public static void ex9() {
+        System.out.println("Ex. 9");
+    }
+    public static void ex10() {
+        System.out.println("Ex. 10");
     }
 }
