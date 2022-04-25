50264fcfa1fe8a150bde53a6c78779bb4f5928e6
TejPatel11 <tejpatel798@gmail.com>
2022-04-25 14:53:43 -0400

    Ex. 1 and Ex.2

--- src/main/java/com/xpanxion/assignments/groupb/student24.java
+++ src/main/java/com/xpanxion/assignments/groupb/student24.java
@@ -1,20 +1,45 @@
 package com.xpanxion.assignments.groupb;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class student24 {
     public static void main(String[] args) {
-        ex1();
+        ArrayList<Integer> sum = new ArrayList<>();
+        System.out.println("The sum of all the numbers is " + addNumbers(sum));
+        ArrayList<Integer> average = new ArrayList<>();
+        System.out.println("The average of all the numbers is " + avgNumbers(average));
     }
 
-    private static void ex1() {
+    //Exercise NUMBER 1
+    private static Integer addNumbers(ArrayList<Integer>numList) {
 
-        System.out.println("tej");
+        int[] ints = {1,2,3};
+        int sum =0;
+        numList = new ArrayList<Integer>(ints.length);
+        for(int i: ints){
+            numList.add(i);
+        }
+        for(int i=0; i<ints.length; i++){
+            sum +=ints[i];
+        }
+        return sum;
     }
-
-    private static void ex2() {
-        System.out.println("Ex. 2");
-    }
-
-    private static void ex3() {
-        System.out.println("Ex. 3");
+    //EXERCISE NUMBER 2
+    private static Float avgNumbers(ArrayList<Integer> numList) {
+        int[] ints = {1,2,3};
+        float average =0.0F;
+        float total=0;
+        numList = new ArrayList<>(ints.length);
+        for(int i:ints){
+            numList.add(i);
+        }
+        for(int i=0; i< ints.length; i++){
+            total +=ints[i];
+        }
+        average = total/ ints.length;
+        return average;
     }
 }
