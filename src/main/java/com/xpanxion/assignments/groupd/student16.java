712cb3c751b8a1877727ade74e6fd93052df119b
Michael Leahey <michael.leahey1843@outlook.com>
2022-04-25 16:41:43 -0400

    EX3

--- src/main/java/com/xpanxion/assignments/groupd/student16.java
+++ src/main/java/com/xpanxion/assignments/groupd/student16.java
@@ -3,18 +3,25 @@ package com.xpanxion.assignments.groupd;
 import java.io.PrintStream;
 import java.sql.Array;
 import java.util.ArrayList;
+import java.util.DoubleSummaryStatistics;
 
 
 public class student16 {
     public static void main(String[] args) {
+        ex1();
+        ex2();
+        ex3();
+    }
+    private static void ex1(){
         int[] ints = {1, 2, 3};
         ArrayList arrayList = new ArrayList( );
         for (int i = 0; i < ints.length; i++) {
-        arrayList.add(ints [i]);
+            arrayList.add(ints [i]);
 
         }
-        System.out.println(average(arrayList));
+        System.out.println(addNumbers(arrayList));
     }
+
     private static Integer addNumbers(ArrayList<Integer> numList)  {
         int sum =0;
         for (int i = 0; i < numList.size(); i++) {
@@ -22,15 +29,15 @@ public class student16 {
         }
         return sum;
     }
-//EX 2
+//EX 2 Begins here
     private static void ex2() {
         int[] ints = {1, 2, 3};
-        ArrayList aL = new ArrayList( );
-        for (float i = average(aL); i < ints.length; i++) {
-
+        ArrayList arrayList = new ArrayList( );
+        for (int i = 0; i < ints.length; i++) {
+            arrayList.add(ints [i]);
 
         }
-        System.out.println(average(aL));
+        System.out.println(average(arrayList));
     }
     private static Float average(ArrayList<Integer> numList)  {
         float avg =0;
@@ -41,8 +48,22 @@ public class student16 {
         avg = sum / numList.size();
         return avg;
     }
-
+//EX3 starts here
     private static void ex3() {
-        System.out.println("Ex. 3");
+        int [] ints = {1,2,3};
+        ArrayList<Integer> num = new ArrayList<Integer>();
+            for (int i = 0; i < ints.length;i++){
+                num.add(ints[i]);
+        }
+            Doubles(num);
+    }
+    private static void Doubles (ArrayList<Integer> numList) {
+        double d = 0;
+        int sum = 0;
+        for(int i = 0; i < numList.size(); i++) {
+            int temp = numList.get(i);
+            numList.set(i,(temp*2));
+        }
+        System.out.println(numList);
     }
 }
