318e2e4955a8cf00437155e566505f48a0d55f2f
dragoliajr <103143873+dragoliajr@users.noreply.github.com>
2022-04-25 12:04:24 -0400

    ex 1

--- src/main/java/com/xpanxion/assignments/groupd/student7.java
+++ src/main/java/com/xpanxion/assignments/groupd/student7.java
@@ -1,12 +1,21 @@
 package com.xpanxion.assignments.groupd;
 
+import java.util.Scanner;
+
 public class student7 {
     public static void main(String[] args) {
+
         ex1();
     }
 
-    private static void ex1() {
+    private static void ex1() { //turn a person name into all uppercase
         System.out.println("Ex. 1");
+        System.out.print("Enter Name: ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println(name.toUpperCase());
+
+
     }
 
     private static void ex2() {
