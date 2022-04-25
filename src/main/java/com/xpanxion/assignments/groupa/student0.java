6fea61185f7d1b3e00a5ed7b311d063d8f2e2215
Martin Burolla <mburolla@xpanxion.com>
2022-04-25 11:07:29 -0400

    update

--- src/main/java/com/xpanxion/assignments/groupa/student0.java
+++ src/main/java/com/xpanxion/assignments/groupa/student0.java
@@ -1,12 +1,18 @@
 package com.xpanxion.assignments.groupa;
 
+import java.util.ArrayList;
+
 public class student0 {
     public static void main(String[] args) {
         ex1();
     }
 
     private static void ex1() {
-        System.out.println("Ex. 1");
+
+        int[] ints = {1, 2, 3};
+
+        var r = addNumbers(null);
+        System.out.println(r);
     }
 
     private static void ex2() {
@@ -16,4 +22,10 @@ public class student0 {
     private static void ex3() {
         System.out.println("Ex. 3");
     }
+
+    private static Integer addNumbers(ArrayList<Integer> numList) {
+        // TODO: Implement this function.
+        return 0;
+    }
+
 }
