4036a7e4fc481f1515b125ca41197e73b9297c09
DominicDiMarcello <103143836+DominicDiMarcello@users.noreply.github.com>
2022-04-25 16:08:55 -0400

    Exercise 3

--- src/main/java/com/xpanxion/assignments/groupb/student8.java
+++ src/main/java/com/xpanxion/assignments/groupb/student8.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class student8 {
     public static void main(String[] args) {
-        ex2();
+        ex3();
 
     }
 
@@ -17,7 +17,10 @@ public class student8 {
         var numList = new ArrayList<Integer>();
         for (int i = 0; i < ints.length; i++) {
             numList.add(ints[i]);
-            sum = sum + ints[i];
+        }
+
+        for (int i = 0; i < numList.size(); i++) {
+            sum = sum + numList.get(i);
         }
 
         System.out.println(numList);
@@ -34,8 +37,11 @@ public class student8 {
         var numList = new ArrayList<Integer>();
         for (int i = 0; i < ints.length; i++) {
             numList.add(ints[i]);
-            sum = sum + ints[i];
-            avg = sum / ints.length;
+        }
+
+        for (int i = 0; i < numList.size(); i++) {
+            sum = sum + numList.get(i);
+            avg = sum / numList.size();
         }
 
         System.out.println(numList);
@@ -45,6 +51,16 @@ public class student8 {
 
     // EX 3
     private static void ex3() {
+
+        int[] ints = {1, 2, 3};
+
+        var numList = new ArrayList<Integer>();
+        for (int i = 0; i < ints.length; i++) {
+            numList.add(ints[i] * 2);
+        }
+
+        System.out.println(Arrays.toString(ints));
+        System.out.println(numList);
 
     }
 
