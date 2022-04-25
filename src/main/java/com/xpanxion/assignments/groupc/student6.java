5fb6d1ced63040083e917a0c0a7a8666ad1e2c9c
daltonhutt <103676585+daltonhutt@users.noreply.github.com>
2022-04-25 14:58:09 -0400

    update

--- src/main/java/com/xpanxion/assignments/groupc/student6.java
+++ src/main/java/com/xpanxion/assignments/groupc/student6.java
@@ -1,12 +1,18 @@
 package com.xpanxion.assignments.groupc;
 
+import java.util.ArrayList;
+
 public class student6 {
     public static void main(String[] args) {
         ex1();
     }
 
     private static void ex1() {
-        System.out.println("DH");
+        int[] ints = {1, 2, 3};
+        // But conversion here...
+
+        var r = addNumbers(null); // how to convert int[] to an ArrayList<Integer>.
+        System.out.println(r);
     }
 
     private static void ex2() {
@@ -16,4 +22,10 @@ public class student6 {
     private static void ex3() {
         System.out.println("Ex. 3");
     }
+
+    private static Integer addNumbers(ArrayList<Integer> numList) {
+        // TODO: Implement this function.
+        return 0;
+    }
+
 }
