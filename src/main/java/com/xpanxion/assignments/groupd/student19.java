634f0c027f5ca2722f1abd7dc748ae59807ce170
raji <rmallina@xpanxion.com>
2022-04-25 15:30:22 -0400

    Ex2

--- src/main/java/com/xpanxion/assignments/groupd/student19.java
+++ src/main/java/com/xpanxion/assignments/groupd/student19.java
@@ -5,18 +5,25 @@ import java.util.Collections;
 import java.util.Scanner;
 
 public class student19 {
+
     public static void main(String[] args) {
 
+        //ex1();
+        ex2();
+
+    }
+
+    private static void ex1(){
         int[] ints = {1, 2, 3};
         ArrayList<Integer> num = new ArrayList<Integer> ();
 
         for( int i = 0; i < ints.length; i++) {
             num.add(ints[i]);
         }
-
         System.out.println(addNumbers(num));
-
     }
+
+
     private static Integer addNumbers(ArrayList<Integer> numList) {
         // TODO: Implement this function.
         int sum = 0;
@@ -28,11 +35,32 @@ public class student19 {
 
 
         private static void ex2() {
+            int[] ints = {1, 2, 3};
+            ArrayList<Integer> num = new ArrayList<Integer>();
+
+            for (int i = 0; i < ints.length; i++) {
+                num.add(ints[i]);
+            }
+            System.out.println(avgNumbers(num));
+        }
+        private static Float avgNumbers(ArrayList<Integer> numList){
+            // TODO: Implement this function.
+            float f = 0;
+            int sum = 0;
+            for(int i=0; i< numList.size();i++) {
+                sum += numList.get(i);
+            }
+           f = sum / numList.size();
+            return f;
 
 
-        System.out.println("Ex. 2");
+        }
 
-    }
+
+
+
+
+
 
     private static void ex3() {
 
