6c88dc50e452f1d4ec413bc73bec59d2d14576c5
Shilpa Sivarajan <shilpa.vchand@gmail.com>
2022-04-25 15:31:26 -0400

    Ex1 & Ex2

--- src/main/java/com/xpanxion/assignments/groupe/student23.java
+++ src/main/java/com/xpanxion/assignments/groupe/student23.java
@@ -1,19 +1,43 @@
 package com.xpanxion.assignments.groupe;
 
-public class student23 {
+import java.util.ArrayList;
+
+public class student23Java1Intro {
     public static void main(String[] args) {
-        ex1();
+        Ex1();
+        Ex2();
     }
 
-    private static void ex1() {
-        System.out.println("Shilpa");
+    private static void Ex1(){
+        int[] ints = {1,2,3};
+       ArrayList<Integer> sumofcharacters = ;
+
+        for(int i = 0; i < ints.length; i++) {
+            sumofcharacters = sumofcharacters + ints[i];
+        }
+        System.out.println(sumofcharacters);
     }
 
-    private static void ex2() {
-        System.out.println("Ex. 2");
+    private static void Ex2(){
+        int[] ints = {1,2,3};
+        var sumofcharacters = 0;
+
+        for(int i = 0; i < ints.length; i++) {
+            sumofcharacters = sumofcharacters + ints[i];
+        }
+        System.out.println(sumofcharacters/ints.length);
+
     }
 
-    private static void ex3() {
-        System.out.println("Ex. 3");
+    private static void Ex3(ArrayList<Integer> numList){
+        ArrayList<Integer> newNumList = new ArrayList<Integer>();
+        Integer i;
+        for ( i =0; i < numList.size(); i++ )
+            newNumList.get(i) = numList.get(i) * 2;
+        System.out.println(newNumList);
+
     }
+
 }
+}
+
