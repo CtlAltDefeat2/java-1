aac3aa4e31960d30d83b1f91971c40b3b1f50c5e
katie <kcohen015@gmail.com>
2022-04-25 16:04:11 -0400

    exercise1 update

--- src/main/java/com/xpanxion/assignments/groupf/student14.java
+++ src/main/java/com/xpanxion/assignments/groupf/student14.java
@@ -1,20 +1,40 @@
 package com.xpanxion.assignments.groupf;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class student14 {
     public static void main(String[] args) {
         ex1();
     }
 
     private static void ex1() {
-        // TODO...
-        System.out.println("Katie.");
+        int[] ints = {1, 2, 3}; //declare array
+
+        ArrayList<Integer> numList = new ArrayList<>();//declare new array list
+        //Implement function
+        for (int i: ints){
+            numList.add(i);
+        }
+             var sum = addNumbers(numList);
+        System.out.println("The sum is " + sum);
     }
 
-    private static void ex2() {
-        System.out.println("Ex. 2");
-    }
-
-    private static void ex3() {
-        System.out.println("Ex. 3");
+    private static Integer addNumbers(ArrayList<Integer> numList) {
+        // implementing function
+        int[] ints = {1, 2, 3}; //declare array
+        int sum = 0;
+        for (int i=0; i< ints.length; i++){
+            sum = sum + ints[i];
+                }
+        return sum;
     }
 }
+//        private static void ex2 () {
+//            System.out.println("Ex. 2");
+//        }
+//
+//        private static void ex3 () {
+//            System.out.println("Ex. 3");
+//        }
+  //
