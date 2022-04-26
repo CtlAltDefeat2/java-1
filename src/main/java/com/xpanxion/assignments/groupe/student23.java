7a90a6e0c6c073db243968aff3f18279bca9d84f
Shilpa Sivarajan <shilpa.vchand@gmail.com>
2022-04-26 15:54:46 -0400

    Ex3

--- src/main/java/com/xpanxion/assignments/groupe/student23.java
+++ src/main/java/com/xpanxion/assignments/groupe/student23.java
@@ -1,62 +1,43 @@
 package com.xpanxion.assignments.groupe;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
 
-public class student23{
+public class student23 {
     public static void main(String[] args) {
-//          var numList = new ArrayList<Integer>();
-//          Scanner scanner = new Scanner(System.in);
-//        while(scanner.hasNextInt()){
-//           numList.add(scanner.nextInt());
-//        }
-//        Integer[] ints = new  Integer[numList.size()];
-//        var newList = new ArrayList<Integer>();
-//        for (int i : ints) {
-//            newList.add(i);
-//        }
-         Ex1();
-         Ex2();
-//         Ex3(newList);
-
-//        var newList = new ArrayList<Integer>();
-//        newList.add(5150);
-//        newList.set(1, 5151);
-//        var r = newList.get(0); // Give me the first item list.
-//        System.out.println(r);
+        Ex1();
+        Ex2();
+        Ex3();
     }
 
-    private static void Ex1(){
-        int[] ints = {1,2,3,4};
+    private static void Ex1() {
+        int[] ints = {1, 2, 3, 4};
         var newList = new ArrayList<Integer>();
-        for (int i : ints) {
+        for (int i : ints)
             newList.add(i);
-        }
+
         var result = addNumbers(newList);
         System.out.println(result);
     }
 
-    private static void Ex2(){
-        int[] ints = {1,2,3,4};
+    private static void Ex2() {
+        int[] ints = {1, 2, 3, 4};
         var newList = new ArrayList<Integer>();
-        for (int i : ints) {
+        for (int i : ints)
             newList.add(i);
-        }
+
         var result = avgNumbers(newList);
         System.out.println(result);
-
-
     }
 
-//   private static void Ex3(ArrayList<Integer>numList){
-//       int[] ints = {1,2,3,4};
-//       var newList = new ArrayList<Integer>();
-//       for (int i : ints) {
-//            newList.add(i);
-//        }
-//        doubleArray(newList);
-//    }
+    private static void Ex3() {
+        int[] ints = {10, 20, 30, 40};
+        var newList = new ArrayList<Integer>();
+        for (int i : ints)
+            newList.add(i);
+
+        var result = doubleArray(newList);
+        System.out.println(result);
+    }
 
     private static Integer addNumbers(ArrayList<Integer> numList) {
         Integer retVal = 0;
@@ -65,19 +46,19 @@ public class student23{
         }
         return retVal;
     }
+
     private static Float avgNumbers(ArrayList<Integer> numList) {
-        // TODO: Implement this function.
         Float Average;
-        Average = Float.valueOf(addNumbers(numList))/numList.size();
-        return(Average);
+        Average = Float.valueOf(addNumbers(numList)) / numList.size();
+        return (Average);
     }
 
-//    private static void doubleArray(ArrayList<Integer> numList){
-//        var newList = new ArrayList<Integer>();
-//        for (int i : numList) {
-//           newList.add(i*2);
-//        }
-//       System.out.println(newList);
-//    }
+    private static ArrayList<Integer> doubleArray(ArrayList<Integer> numList) {
+        for (int i = 0; i < numList.size(); i++) {
+            numList.set(i, numList.get(i) * 2);
+        }
+        return numList;
+    }
+
 }
 
