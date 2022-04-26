ae592ca83ac298e186ddff48dfe0bb578012269b
Elden-Lord01 <levi.melton43@gmail.com>
2022-04-26 10:53:59 -0400

    updae.

--- src/main/java/com/xpanxion/assignments/groupa/student20.java
+++ src/main/java/com/xpanxion/assignments/groupa/student20.java
@@ -5,20 +5,30 @@ import java.util.ArrayList;
 public class student20 {
     public static void main(String[] args) {
         ex1();
+        ex2();
+        ex3();
+        ex4();
+        ex5();
+        ex6();
+        ex7();
+//        ex8();
+//        ex9();
+//        ex10();
     }
 
     private static void ex1() {
-
-        int[] ints = {1, 2, 3};
-
-        var r = addNumbers( null);
-        System.out.println(r);
-
+        System.out.println("Ex. 1");
+        addNumbers(new ArrayList<>());
     }
-
-
-    private static void ex2() {
-        System.out.println("Ex. 2");
+    private static Integer addNumbers(ArrayList<Integer> numList) {
+        int[] ints = {1, 2, 3};
+        int sum = 0;
+        var list1 = new ArrayList<Integer>();
+        for (int i = 0; i < ints.length; i++){
+            list1.add(ints[i]);
+            sum += list1.get(i);
+        }
+        System.out.println("Sum of all numbers in Array: " + sum);
     }
 
     private static void ex3() {
@@ -26,6 +36,10 @@ public class student20 {
     }
 
     private static int addNumbers(ArrayList<Integer> numList) {
+        // TODO: Implement this function.
+        return 0;
+    }
+    { private static Float avgNumbers(ArrayList<Integer> numList)
         // TODO: Implement this function.
         return 0;
     }
