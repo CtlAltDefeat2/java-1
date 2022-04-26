f66adebb09467a727c36b8dbe95a0913bd426c28
Brianprice098 <103675908+Brianprice098@users.noreply.github.com>
2022-04-26 09:56:05 -0400

    "test commit"

--- src/main/java/com/xpanxion/assignments/groupa/student3.java
+++ src/main/java/com/xpanxion/assignments/groupa/student3.java
@@ -1,11 +1,15 @@
 package com.xpanxion.assignments.groupa;
-
 public class student3 {
-    public static void main(String[] args) {
-        ex1();
-    }
+    public static void main(String[] args) {ex1();
+
+        }
+
 
     private static void ex1() {
+        int[] ints = {1, 2, 3};
+        private static Integer addNumbers(ArrayList(1, 2, 3)numList) {
+            // TODO: Implement this function.
+            return 0;
         System.out.println("Ex. 1");
     }
 
