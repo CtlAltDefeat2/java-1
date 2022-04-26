6494b6fb0f67df2c25780bd0e60eaf76cacb20b3
chidentz <chidentzovinsky@gmail.com>
2022-04-26 12:49:07 -0500

    Exercice 2

--- src/main/java/com/xpanxion/assignments/groupa/student4.java
+++ src/main/java/com/xpanxion/assignments/groupa/student4.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class student4 {
     public static void main(String[] args) {
         ex1();
+        ex2();
     }
 
     private static Integer addNumbers(ArrayList<Integer> numList) { // 1,2,3,4
@@ -16,18 +17,35 @@ public class student4 {
     }
 
     private static void ex1() {
-        int[] ints = {1, 2, 3,4};
+        int[] ints = {1, 2, 3, 4};
         var newList = new ArrayList<Integer>();
-        for (int i : ints){
+        for (int i : ints) {
             newList.add(i);
         }
         var result = addNumbers(newList);
-        System.out.println(result);
+        System.out.println("the sum of the Array is: " + result);
     }
 
+
+
+
     private static void ex2() {
-        System.out.println("Ex. 2");
+        int [] ints2 = {1,2,3,};
+        var newlist2 = new ArrayList<Integer>();
+        for (int b :ints2 ) {
+            newlist2.add(b);
+        }
+        var result =avgNumbers(newlist2);
+        System.out.println("average numbers is " + result);
+        }
+    private static float avgNumbers(ArrayList<Integer> numList) { // 1,2,3,4
+        float retval = 0;
+        for (Integer i : numList) { // For each Integer "i" in this numList, I'm going to do the following...
+            retval = retval + i; // = is an assignment operator.
+        }
+        return retval;
     }
+
 
     private static void ex3() {
         System.out.println("Ex. 3");
