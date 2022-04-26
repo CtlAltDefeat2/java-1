0ab7dc3f768a07df06f8e6690b1f845ed54e7fd9
chidentz <chidentzovinsky@gmail.com>
2022-04-26 12:08:19 -0500

    update

--- src/main/java/com/xpanxion/assignments/groupa/student4.java
+++ src/main/java/com/xpanxion/assignments/groupa/student4.java
@@ -1,12 +1,28 @@
 package com.xpanxion.assignments.groupa;
 
+import java.util.ArrayList;
+
 public class student4 {
     public static void main(String[] args) {
         ex1();
     }
 
+    private static Integer addNumbers(ArrayList<Integer> numList) { // 1,2,3,4
+        Integer retval = 0;
+        for (Integer i : numList) { // For each Integer "i" in this numList, I'm going to do the following...
+            retval = retval + i; // = is an assignment operator.
+        }
+        return retval;
+    }
+
     private static void ex1() {
-        System.out.println("Ex. 1");
+        int[] ints = {1, 2, 3,4};
+        var newList = new ArrayList<Integer>();
+        for (int i : ints){
+            newList.add(i);
+        }
+        var result = addNumbers(newList);
+        System.out.println(result);
     }
 
     private static void ex2() {
