7d704be5835b82217c00201be6081e4cddc14529
Birk <rgeebt01@yahoo.com>
2022-04-26 14:54:20 -0400

    checked by mb and cleaned up some

--- src/main/java/com/xpanxion/assignments/groupa/Student1.java
+++ src/main/java/com/xpanxion/assignments/groupa/Student1.java
@@ -18,69 +18,48 @@ public class Student1 {
     public static void ex1() {
         System.out.println("EX. 1");
         int[] ints = {1, 2, 3};// integer array
-        ArrayList<Integer> numList = (ArrayList<Integer>) Arrays.stream(ints).boxed().collect(Collectors.toList());//convert integer array to ArrayList
-        System.out.println(numList);//print out numList
-        addNumbers(numList);
+        ArrayList<Integer> numList = (ArrayList<Integer>) Arrays.stream(ints).boxed().collect(Collectors.toList());//convert integer array to ArrayList using stream
+        var result=addNumbers(numList);//call addNumbers to get result
+        System.out.println("The sum of the ArrayList is: "+result);
     }
     private static Integer addNumbers(ArrayList<Integer> numList) {
-        System.out.println(numList);
-        Integer sum=0;//declare variable sum outside of block
+        Integer sum=0;//declare variable sum outside of following for loop block
         for (int i:numList) {//enhanced for loop to get sum of ArrayList
             sum+=i;
         }
-        System.out.println(sum);
-        // TODO: Implement this function.
         return sum;
     }
-//        System.out.println("Average = "+average);
-//        System.out.println(ints);
 
-//        Scanner myKeyBoard= new Scanner(System.in);//initialize scanner outside of block
-//        ArrayList<Integer> numList= new ArrayList<>();//initialize array variable outside of block
-//        System.out.println("Please enter a nonzero integer to be added (enter 0 when done)");//ask initial question
-//        Integer targetNum= myKeyBoard.nextInt();//get input from user
-//        while (true) {
-//            if (targetNum==0) {
-//                System.out.println("The last number has been added");
-//                break;
-//            } else {
-//                numList.add(targetNum);
-//                System.out.println("Enter another nonzero interger to be added (enter 0 when done)");
-//                targetNum=myKeyBoard.nextInt();
-//            }
-//        int sum=0;
-//        for (int number:numList){
-//            sum+=number;
-//        }
-//        System.out.println(sum);
-//    }
-
-    private static void ex2() {
+    public static void ex2() {
         System.out.println("Ex. 2");
-        int[] intArray={1, 2, 3};
-        ArrayList<Integer> intArrayList = (ArrayList<Integer>) Arrays.stream(intArray).boxed().collect(Collectors.toList());
-        avgNumbers(intArrayList);
+        int[] intArray={1, 2, 3};//declare the int array outside of function
+        ArrayList<Integer> intArrayList = (ArrayList<Integer>) Arrays.stream(intArray).boxed().collect(Collectors.toList());//convert the intArray to intArrayList using stream
+        var resultAvg=avgNumbers(intArrayList);//call avgNumbers to get resultAvg
+        System.out.println("The Average is: "+resultAvg);
 
     }
     private static Float avgNumbers(ArrayList<Integer> numList) {
-        int[] intArray={1,2,3};
-        float average = (float) numList.stream().mapToInt(value -> value).average().orElse(0.0);
-
-        System.out.println("Average = " + average);
-        // TODO: Implement this function.
+        float average = (float) numList.stream().mapToInt(value -> value).average().orElse(0.0);//convert get average of previously declared numList using stream
         return average;
     }
 
-    private static void ex3() {
+    public static void ex3() {
         System.out.println("Ex. 3");
         doubleArrayList(new ArrayList<>());
-
     }
     private static void doubleArrayList(ArrayList<Integer> numList){
         List<Integer> intsArray=Arrays.asList(1,2,3);
         List<Integer> doubleNumList= intsArray.stream().map(i -> i * 2).toList();
         System.out.println("Double values of numList: "+doubleNumList);
     }
+    public static void ex4(){
+        System.out.println("Ex. 4");
+
+    }
+    private static void createList(Integer maxNumber){
+
+    }
+
 
 
 }
