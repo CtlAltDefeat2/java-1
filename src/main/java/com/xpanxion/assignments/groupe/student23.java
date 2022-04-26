a42699eb8d74b86f7a4796d4b399d4a229e944bc
Shilpa Sivarajan <shilpa.vchand@gmail.com>
2022-04-26 13:01:25 -0400

    Ex1 & Ex2

--- src/main/java/com/xpanxion/assignments/groupe/student23.java
+++ src/main/java/com/xpanxion/assignments/groupe/student23.java
@@ -7,8 +7,14 @@ import java.util.Scanner;
 public class student23{
     public static void main(String[] args) {
 
-        Ex1();
+         Ex1();
+         Ex2();
 
+//        var newList = new ArrayList<Integer>();
+//        newList.add(5150);
+//        newList.set(1, 5151);
+//        var r = newList.get(0); // Give me the first item list.
+//        System.out.println(r);
     }
 
     private static void Ex1(){
@@ -22,11 +28,19 @@ public class student23{
     }
 
     private static void Ex2(){
+        int[] ints = {1,2,3,4};
+        var newList = new ArrayList<Integer>();
+        for (int i : ints) {
+            newList.add(i);
+        }
+        var result = avgNumbers(newList);
+        System.out.println(result);
 
 
     }
 
     private static void Ex3(ArrayList<Integer>numList){
+
 
 
     }
@@ -37,6 +51,20 @@ public class student23{
             retVal = retVal + i;
         }
         return retVal;
+    }
+    private static Float avgNumbers(ArrayList<Integer> numList) {
+        // TODO: Implement this function.
+        Float Average;
+        Average = Float.valueOf(addNumbers(numList)/numList.size());
+        return(Average);
+    }
+
+    private static void double(ArrayList<Integer> numList){
+        int[] ints = {1,2,3,4};
+        var newList = new ArrayList<Integer>();
+        for (int i : ints) {
+            newList.add(i*2);
+        }
     }
 }
 
