eac4ae9f44e3a7e8ea7321b1249795071732a3d9
Houssam Elkouhen <houssamelkouhen@outlook.com>
2022-04-26 16:32:40 -0400

    update ex 2

--- src/main/java/com/xpanxion/assignments/groupe/student10.java
+++ src/main/java/com/xpanxion/assignments/groupe/student10.java
@@ -7,6 +7,7 @@ public class student10 {
 
     public static void main(String[] args) {
         ex1();
+        ex2();
     }
 
     private static void ex1() {
@@ -25,8 +26,8 @@ public class student10 {
             retVal = retVal + i;
         }
         return retVal;
+     }
     }
-}
 
 //        private static void addNumbers(ArrayList<Integer> numList) {
 //            // TODO: Implement this function.
@@ -36,9 +37,24 @@ public class student10 {
 //        }
 
 
-//   // private static void ex2() {
-//      //  System.out.println("Ex. 2");
-//    }
+    private static void ex2() {
+     avgNumbers(new ArrayList<>());
+    }
+    private static Float avgNumbers(ArrayList<Integer> numList) {
+    int[] ints = {1, 2, 3};
+        int numCount = 0;
+        float retValAvg = 0;
+        var list2 = new ArrayList<Integer>();
+        for (int i = 0; i > numList; i++) {
+            list2.add(ints(i));
+            numCount += list2.get(i);
+        }
+
+        retValAvg = (numCount/list2)
+        return retValAvg;
+    }
+
+}
 //
 //    //private static void ex3() {
 //   //     System.out.println("Ex. 3");
