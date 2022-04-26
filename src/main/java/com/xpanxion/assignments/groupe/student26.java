2a99288d6dcd89756e3be2b333db2e95499f49f4
Zachary Porras <ztp567@gmail.com>
2022-04-26 14:30:05 -0400

    update

--- src/main/java/com/xpanxion/assignments/groupe/student26.java
+++ src/main/java/com/xpanxion/assignments/groupe/student26.java
@@ -1,16 +1,56 @@
 package com.xpanxion.assignments.groupe;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class student26 {
     public static void main(String[] args) {
+
         ex1();
+
     }
 
     private static void ex1() {
-        System.out.println("Ex. 1");
+
+        //Test writing in code
+        int[] ints = {1, 2, 3};
+        var newList = new ArrayList<Integer>();
+        // TODO: convert ints to newList.
+
+        for (int i:ints) {
+            newList.add(i);
+        }
+
+
+        var result = addNumbers(newList);
+        System.out.println(result);
+
     }
 
+    private static  Integer addNumbers(ArrayList<Integer>numList) {
+
+        //TODO:Implement this function
+        Integer retval=0;
+
+        for (Integer i :numList) {
+            retval =  retval + i;
+        }
+        return  retval;
+
+    }
+
+
+
     private static void ex2() {
-        System.out.println("Ex. 2");
+
+
+
+
+
+
+
     }
 
     private static void ex3() {
