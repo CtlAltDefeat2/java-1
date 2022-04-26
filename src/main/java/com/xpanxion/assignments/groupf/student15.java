649e19c35f456634174f71b403da5c19b09c3382
PKavya1792 <103611194+PKavya1792@users.noreply.github.com>
2022-04-26 14:03:02 -0400

    update ex2

--- src/main/java/com/xpanxion/assignments/groupf/student15.java
+++ src/main/java/com/xpanxion/assignments/groupf/student15.java
@@ -8,8 +8,10 @@ import java.util.stream.IntStream;
     public class student15 {
         public static void main(String[] args) {
             ex1();
+            ex2();
         }
-        private static void ex1(){
+
+        private static void ex1() {
             int[] ints = {1, 2, 3};
             var numList = new ArrayList<Integer>(ints.length);
             for (int i : ints) {
@@ -18,20 +20,39 @@ import java.util.stream.IntStream;
             var sum = addNumbers(numList);
             System.out.println("The sum is " + sum);
         }
-        private static Integer addNumbers(ArrayList<Integer> numList){
-                        int sum=0;
-            for (int i=0;i<numList.size();i++) {
-                sum+= numList.get(i);
+
+        private static Integer addNumbers(ArrayList<Integer> numList) {
+            int sum = 0;
+            for (int i = 0; i < numList.size(); i++) {
+                sum += numList.get(i);
             }
-            return sum;}}
+            return sum;
+        }
 
-            //
+        private static void ex2() {
+            int[] ints = {1, 2, 3, 4};
+            // Define an ArrayList of integers.
+            int length = ints.length;
+            var newList = new ArrayList<Integer>();
+            // Create for loop to add integers.
+            for (int i : ints) {
+                newList.add(i);
+            }
+            var r = avgNumbers(newList);
+            System.out.println(r);
+        }
 
-//
-//    private static void ex2() {
-//        System.out.println("Ex. 2");
-//    }
-////
+        private static Float avgNumbers(ArrayList<Integer> numList) {
+            Float retVal = 0F;
+            var sum = addNumbers(numList);
+
+            float length=numList.size();
+            float average = sum/length;
+            return average;
+
+                }}
+
+
 //    private static void ex3() {
 //        System.out.println("Ex. 3");
 //    }
