bdd8d366b999febdffeaa9d4e7457240000fb23f
chidentz <chidentzovinsky@gmail.com>
2022-04-26 14:04:27 -0500

    Exercice 2

--- src/main/java/com/xpanxion/assignments/groupa/student4.java
+++ src/main/java/com/xpanxion/assignments/groupa/student4.java
@@ -1,11 +1,14 @@
 package com.xpanxion.assignments.groupa;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class student4 {
     public static void main(String[] args) {
         ex1();
         ex2();
+        ex3();
     }
 
     private static Integer addNumbers(ArrayList<Integer> numList) { // 1,2,3,4
@@ -30,24 +33,19 @@ public class student4 {
 
 
     private static void ex2() {
-        int [] ints2 = {1,2,3,};
-        var newlist2 = new ArrayList<Integer>();
-        for (int b :ints2 ) {
-            newlist2.add(b);
+        List<Integer> list = Arrays.asList(1, 2, 3, 4);
+        double sum = 0;
+        for (int i : list) {
+            sum += i;
         }
-        var result =avgNumbers(newlist2);
-        System.out.println("average numbers is " + result);
-        }
-    private static float avgNumbers(ArrayList<Integer> numList) { // 1,2,3,4
-        float retval = 0;
-        for (Integer i : numList) { // For each Integer "i" in this numList, I'm going to do the following...
-            retval = retval + i; // = is an assignment operator.
-        }
-        return retval;
+        double average = sum / list.size();
+
+        System.out.println("Average = " + average);
     }
 
 
+
     private static void ex3() {
-        System.out.println("Ex. 3");
+
     }
 }
