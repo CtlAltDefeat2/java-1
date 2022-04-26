eec6451dcbe167ec11f9997153b20f8061a36c27
Shilpa Sivarajan <shilpa.vchand@gmail.com>
2022-04-26 09:29:32 -0400

    Trial

--- src/main/java/com/xpanxion/assignments/groupe/student23.java
+++ src/main/java/com/xpanxion/assignments/groupe/student23.java
@@ -1,21 +1,36 @@
 package com.xpanxion.assignments.groupe;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class student23{
     public static void main(String[] args) {
+//        int[] numList = new int[5];
+//        Scanner scanner = new Scanner(System.in);
         Ex1();
-        Ex2();
+//        Ex2();
+//        System.out.println("Enter 5 numbers: " );
+//        for(int i = 0; i < 5; i++) {
+//            numList[i] = scanner.nextInt();
+//        }
+//        Ex3(numList.);
     }
 
     private static void Ex1(){
         int[] ints = {1,2,3};
-        var sumofcharacters = 0;
+//        var sumofcharacters = 0;
+//
+//        for(int i = 0; i < ints.length; i++) {
+//            sumofcharacters = sumofcharacters + ints[i];
+//        }
 
-        for(int i = 0; i < ints.length; i++) {
-            sumofcharacters = sumofcharacters + ints[i];
-        }
-        System.out.println(sumofcharacters);
+        // Convert int[] to ArrayList<Integer>
+
+
+
+        var r = addNumbers(???);
+        System.out.println(r);
     }
 
     private static void Ex2(){
@@ -29,14 +44,21 @@ public class student23{
 
     }
 
- /*   private static void Ex3(ArrayList<Integer> numList){
+    private static void Ex3(ArrayList<Integer>numList){
         ArrayList<Integer> newNumList = new ArrayList<Integer>();
-        Integer i;
-        for ( i =0; i < numList.size(); i++ )
-            newNumList.get(i) = numList.get(i) * 2;
-
+        int num;
+        //Integer i;
+        for (int i : numList) {
+            num = numList.get(i) *2; // gets a number from the list // = numList.get(i) * 2;
+            newNumList.set(i, num); // updates the list
+        }
         System.out.println(newNumList);
 
-    }*/
+    }
+
+    private static Integer addNumbers(ArrayList<Integer> numList) {
+        // TODO: Implement this function.
+        return 0;
+    }
 }
 
