1bc6ea2d165d015b071c03b8a9576cfb57468e05
wdelacruzl <wdelacruzl98@gmail.com>
2022-04-26 14:02:32 -0400

    Ex 2

--- src/main/java/com/xpanxion/assignments/groupf/student25.java
+++ src/main/java/com/xpanxion/assignments/groupf/student25.java
@@ -1,11 +1,15 @@
 package com.xpanxion.assignments.groupf;
 
+import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class student25 {
     public static void main(String[] args) {
+
         ex1();
+        ex2();
     }
 
     private static void ex1() {
@@ -17,26 +21,45 @@ public class student25 {
         var sum = addNumbers(numList);
         System.out.println("The sum is " + sum);
     }
-        private static Integer addNumbers(ArrayList<Integer> numList) {
-            int sum = 0;
-            for(int i = 0; i < numList.size(); i++){
-                sum += numList.get(i);
-            }
-            return sum;
 
+    private static Integer addNumbers(ArrayList<Integer> numList) {
+        int sum = 0;
+        for (int i = 0; i < numList.size(); i++) {
+            sum += numList.get(i);
         }
+        return sum;
+
+    }
 
     private static void ex2() {
-        int[] ints = {1, 2, 3};
 
-
-
-
-
-            System.out.println("Ex. 2");
+        int[] ints = {1, 2, 3, 4};
+        // Define an ArrayList of integers.
+        var newList = new ArrayList<Integer>();
+        // Create for loop to add integers.
+        for (int i : ints) {
+            newList.add(i);
+            // Use "var r" to help the conversion and to print out float.
+        }
+        var r = avgNumbers(newList);
+        System.out.println(r);
     }
 
-    private static void ex3() {
-        System.out.println("Ex. 3");
+    private static Float avgNumbers(ArrayList<Integer> numList) {
+        Float retVal = 0F;
+        var sum = addNumbers(numList);
+        float length = numList.size();
+        float average = sum / length;
+
+        System.out.println("The average of the Array is: " + average);
+
+        return average;
     }
 }
+
+
+
+//    private static void ex3() {
+//        System.out.println("Ex. 3");
+//    }
+//}}
