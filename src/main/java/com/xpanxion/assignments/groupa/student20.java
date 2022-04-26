2df440ee158d3bb5981ca1f6c37e5a0d0c45bb21
Elden-Lord01 <levi.melton43@gmail.com>
2022-04-26 15:11:20 -0400

    EXC 1 done.

--- src/main/java/com/xpanxion/assignments/groupa/student20.java
+++ src/main/java/com/xpanxion/assignments/groupa/student20.java
@@ -2,18 +2,13 @@ package com.xpanxion.assignments.groupa;
 
 import java.util.ArrayList;
 
+import static com.xpanxion.assignments.groupa.student20.main;
+
 public class student20 {
     public static void main(String[] args) {
         ex1();
-        ex2();
-        ex3();
-        ex4();
-        ex5();
-        ex6();
-        ex7();
-//        ex8();
-//        ex9();
-//        ex10();
+
+
     }
 
     private static void ex1() {
@@ -24,23 +19,16 @@ public class student20 {
         int[] ints = {1, 2, 3};
         int sum = 0;
         var list1 = new ArrayList<Integer>();
-        for (int i = 0; i < ints.length; i++){
+        for (int i = 0; i < ints.length; i++) {
             list1.add(ints[i]);
             sum += list1.get(i);
         }
         System.out.println("Sum of all numbers in Array: " + sum);
+        return null;
+    }
     }
 
-    private static void ex3() {
-        System.out.println("Ex. 3");
-    }
 
-    private static int addNumbers(ArrayList<Integer> numList) {
-        // TODO: Implement this function.
-        return 0;
-    }
-    { private static Float avgNumbers(ArrayList<Integer> numList)
-        // TODO: Implement this function.
-        return 0;
-    }
-}
+
+
+
