aa9f8fec9d84a740bf5eb38732ce78366ae8ed3e
Martin Burolla <mburolla@xpanxion.com>
2022-04-26 12:55:02 -0400

    dev-a-mb-java-intro

--- src/main/java/com/xpanxion/assignments/groupa/student0.java
+++ src/main/java/com/xpanxion/assignments/groupa/student0.java
@@ -3,29 +3,36 @@ package com.xpanxion.assignments.groupa;
 import java.util.ArrayList;
 
 public class student0 {
+
+    // Framework: SpringBoot Web API
+    // Collections, data structures, streams, lambda
+    // OOP (classes, interfaces)
+    // vars, loops, ifs, functions  <=============== We are here
+
     public static void main(String[] args) {
         ex1();
     }
 
     private static void ex1() {
-
         int[] ints = {1, 2, 3};
-
-        var r = addNumbers(null);
+        var tempList = new ArrayList<Integer>();
+        for (int i: ints) {
+            tempList.add(i);
+        }
+        var r = addNumbers(tempList);
         System.out.println(r);
     }
 
-    private static void ex2() {
-        System.out.println("Ex. 2");
-    }
-
-    private static void ex3() {
-        System.out.println("Ex. 3");
-    }
+    //
+    // My functions
+    //
 
     private static Integer addNumbers(ArrayList<Integer> numList) {
-        // TODO: Implement this function.
-        return 0;
+        Integer retval = 0;
+        for(Integer i: numList) {
+            retval += i;
+        }
+        return retval;
     }
 
 }
