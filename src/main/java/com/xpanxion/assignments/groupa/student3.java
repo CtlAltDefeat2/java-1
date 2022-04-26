d3972a75da22a1e6720ee983cc13ad98ba807173
Brianprice098 <103675908+Brianprice098@users.noreply.github.com>
2022-04-26 19:25:19 -0400

    finished ex 1

--- src/main/java/com/xpanxion/assignments/groupa/student3.java
+++ src/main/java/com/xpanxion/assignments/groupa/student3.java
@@ -1,20 +1,34 @@
 package com.xpanxion.assignments.groupa;
-
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.stream.*;
 public class student3 {
     public static void main(String[] args) {
         ex1();
     }
 
-    private static void ex1() {System.out.println("Ex. 1");
-        {
-        int my_array[] = {1, 2, 3};
+    private static void ex1() {
+        System.out.println("Ex. 1");
+        int[] arr = {1,2,3}; //has to determine size before
         int sum = 0;
-        for (int i : my_array);
-        sum += i;
-        System.out.println("sum of numbers in array:" + sum);
+        var arrayl = new ArrayList<Integer>(); // can change size
+            for (int i = 0; i < arr.length ; i++) { //while i<arr.length keep doing this
+                arrayl.add(arr[i]);
 
+            }
+        System.out.println("sum of numbers in array:" + addNumbers(arrayl));
+        
         }
-    }
+
+        private static Integer addNumbers(ArrayList<Integer> numList) {
+            var sum = 0;
+            for (int i = 0; i < numList.size() ; i++) {
+                sum += numList.get(i);
+            }
+            // TODO: Implement this function.
+            return sum;
+        }
+
 
 
 
