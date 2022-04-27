9e91870325979acde7d8f7a083b83c458acd9b84
Elden-Lord01 <levi.melton43@gmail.com>
2022-04-27 16:02:12 -0400

    Added ex 2, 3.

--- src/main/java/com/xpanxion/assignments/groupa/student20.java
+++ src/main/java/com/xpanxion/assignments/groupa/student20.java
@@ -26,7 +26,49 @@ public class student20 {
         System.out.println("Sum of all numbers in Array: " + sum);
         return null;
     }
+
+
+    private static void ex2() {
+        System.out.println("Ex. 2");
+        avgNumbers(new ArrayList<>());
     }
+
+    private static Float avgNumbers(ArrayList<Integer> numList) {
+        // TODO: Implement this function.
+        int[] ints = {1, 2, 3};
+        float avg = 0;
+        int sum = 0;
+        var list = (new ArrayList<Integer>());
+        for (int i = 0; i < ints.length; i++){
+            list.add(ints[i]);
+            sum += list.get(i);
+        }
+        avg =sum/ints.length;
+
+        return null;
+    }
+    private static void ex3() {
+        System.out.println("Ex. 3");
+        int[] ints = {1, 2, 3};
+        var list = new ArrayList<Integer>();
+        for (int i = 0; i < ints.length; i++){
+            list.add(ints[i]);
+
+        }
+        multiply(list);
+    }
+    private static void multiply(ArrayList<Integer> list) {
+        for (int i = 0; i < list.size(); i++) {
+            list.set(i, list.get(1) *2);
+        }
+        System.out.println("This is the doubled Array: " + list);
+    }
+    }
+
+
+
+
+
 
 
 
