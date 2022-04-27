c5404c4861f89762402ae8061cde580834ab3d74
Houssam Elkouhen <houssamelkouhen@outlook.com>
2022-04-27 16:13:13 -0400

    update ex2

--- src/main/java/com/xpanxion/assignments/groupe/student10.java
+++ src/main/java/com/xpanxion/assignments/groupe/student10.java
@@ -1,5 +1,6 @@
 package com.xpanxion.assignments.groupe;
 
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.stream.IntStream;
 
@@ -8,6 +9,7 @@ public class student10 {
     public static void main(String[] args) {
         ex1();
         ex2();
+        ex3();
     }
 
     private static void ex1() {
@@ -26,37 +28,33 @@ public class student10 {
             retVal = retVal + i;
         }
         return retVal;
-     }
     }
-
-//        private static void addNumbers(ArrayList<Integer> numList) {
-//            // TODO: Implement this function.
-//            int[] ints = {1, 2, 3};
-//            int sum = IntStream.of(ints).sum();
-//            System.out.println("The sum of all numbers in the array is: " + sum);
-//        }
 
 
     private static void ex2() {
-     avgNumbers(new ArrayList<>());
-    }
-    private static Float avgNumbers(ArrayList<Integer> numList) {
-    int[] ints = {1, 2, 3};
-        int numCount = 0;
-        float retValAvg = 0;
+        int[] ints = {1, 2, 3};
         var list2 = new ArrayList<Integer>();
-        for (int i = 0; i > numList; i++) {
-            list2.add(ints(i));
-            numCount += list2.get(i);
+        for (int i = 0; i < ints.length; i++){
+            list2.add(ints[i]);
         }
-
-        retValAvg = (numCount/list2)
-        return retValAvg;
+        var result = avgNumbers(list2);
+        System.out.println(result);
+    }
+    private static Float avgNumbers(ArrayList<Integer> list2) {
+        int sum = 0;
+        float avg = 0;
+        for (int i = 0; i < list2.size(); i++) {
+            sum += list2.get(1);
+        }
+        avg = sum/list2.size();
+        return avg;
     }
 
+    private static void ex3(){
+        int[] ints = {1, 2, 3};
+        var newList = new ArrayList<Integer>();
+    }
+
+
+   // private static void double(ArrayList<Integer> numList)
 }
-//
-//    //private static void ex3() {
-//   //     System.out.println("Ex. 3");
-//    }
-//}
