d47a214f14ab3282338a1a35fd884550f27b87ef
Zachary Porras <ztp567@gmail.com>
2022-04-27 00:42:30 -0400

    update

--- src/main/java/com/xpanxion/assignments/groupe/student26.java
+++ src/main/java/com/xpanxion/assignments/groupe/student26.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class student26 {
     public static void main(String[] args) {
 
-        ex1();
+        ex2();
 
     }
 
@@ -23,37 +23,52 @@ public class student26 {
             newList.add(i);
         }
 
-
+        //System.out.println(newList);
         var result = addNumbers(newList);
-        System.out.println(result);
+         System.out.println(result);
 
     }
-
-    private static  Integer addNumbers(ArrayList<Integer>numList) {
+     private static  Integer addNumbers(ArrayList<Integer>numList) {
 
         //TODO:Implement this function
-        Integer retval=0;
+          Integer retval=0;
 
-        for (Integer i :numList) {
+         for (Integer i :numList) {
             retval =  retval + i;
         }
-        return  retval;
+         return  retval;
 
-    }
+     }
 
-
+//Example 2 Begin
 
     private static void ex2() {
 
+        int[] ints = {1, 2, 3,12};
+        var listnew = new ArrayList<Integer>();
+        // TODO: convert ints to newList.
+
+        for (int i:ints) {
+            listnew.add(i);
+        }
+
+        var result = avgNumbers(listnew);
+        System.out.println(result);
+
+
+        }
+        private static Float avgNumbers(ArrayList<Integer> numList) {
+            // TODO: Implement this function.
+
+            var retval = addNumbers(numList);
+            return Float.valueOf(retval)/numList.toArray().length;
+
+
+        }
 
 
 
-
-
-
-    }
-
-    private static void ex3() {
+        private static void ex3() {
         System.out.println("Ex. 3");
     }
 }
