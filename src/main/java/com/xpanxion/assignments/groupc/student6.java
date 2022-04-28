56da709646d9797317ac40e8407c529d2aee0d9d
daltonhutt <103676585+daltonhutt@users.noreply.github.com>
2022-04-28 15:57:59 -0400

    Exercise 8 & 9.

--- src/main/java/com/xpanxion/assignments/groupc/student6.java
+++ src/main/java/com/xpanxion/assignments/groupc/student6.java
@@ -1,7 +1,10 @@
 package com.xpanxion.assignments.groupc;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.sql.Array;
 import java.util.*;
+import java.util.random.RandomGenerator;
 
 public class student6 {
     public static void main(String[] args) {
@@ -13,12 +16,63 @@ public class student6 {
         ex6();
         ex7();
         ex8();
+        ex9();
+        ex10();
+    }
+    private static void ex10() {
+
+    }
+   // private static Boolean passChecker() {
+
+//    }
+    private static void ex9() {
+        Random random = new Random();
+        var myArray = new ArrayList<Integer>();
+        for (int i =0; i < 10; i++) {
+            myArray.add(random.nextInt(1,1000));
+        }
+        System.out.print(addNumbers(myArray));
+        var result = checkArray(myArray);
+        System.out.println(" " + result);
+    }
+    private static String checkArray(ArrayList<Integer> myArray) {
+        var result = (addNumbers(myArray) % 2 == 0) ? "even" : "odd";
+        return result;
     }
     private static void ex8() {
-
+        int[] ints = {10, 5, 2};
+        var numList = new ArrayList<Integer>(ints.length);
+        for (int i = 0; i < ints.length; i++)
+        {
+            numList.add(ints[i]);
+        }
+        var price = (addNumbers(numList));
+        var price2 = shipHandFee(price);
+        System.out.println(price2);
     }
-    private static void totalCost() {
-
+    private static Float shipHandFee(int price) {
+        var taxTotal = taxFee(price);
+        float totalPrice = 0.0f;
+        if (taxTotal < 10)
+        {
+            boolean underTen = true;
+            totalPrice = taxTotal + 5;
+            return totalPrice;
+        }
+        else if (taxTotal >= 10 && taxTotal <= 20)
+        {
+            boolean tenToTwenty = true;
+            totalPrice = taxTotal + 4;
+            return totalPrice;
+        }
+        else {totalPrice = taxTotal;
+            return totalPrice;
+        }
+    }
+    private static Float taxFee(int price) {
+        float salesTax = .1f;
+        float taxTotal = price * (salesTax + 1);
+        return taxTotal;
     }
     private static void ex7() {
         int[] ints = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
@@ -75,7 +129,8 @@ public class student6 {
     }
     private static void createList(Integer maxNumber) {
         var numList = new ArrayList<>(5);
-        for (int i = 0; i < maxNumber; i++) {
+        for (int i = 0; i < maxNumber; i++)
+        {
             numList.add(i+1);
         }
         System.out.println(numList);
@@ -90,7 +145,8 @@ public class student6 {
         doubled(numList);
     }
     private static void doubled(ArrayList<Integer> numList) {
-        for (int i = 0; i < numList.size(); i++) {
+        for (int i = 0; i < numList.size(); i++)
+        {
             numList.set(i, numList.get(i) * 2);
         }
         System.out.println(numList);
