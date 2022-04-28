44670573c67379f17eb6862986d76164741c989b
chidentz <chidentzovinsky@gmail.com>
2022-04-28 11:17:02 -0500

    Exercice1 - Exercice 4

--- src/main/java/com/xpanxion/assignments/groupa/student4.java
+++ src/main/java/com/xpanxion/assignments/groupa/student4.java
@@ -1,11 +1,7 @@
 package com.xpanxion.assignments.groupa;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
-import static java.util.stream.Collectors.toList;
+import java.util.Scanner;
 
 public class student4 {
     public static void main(String[] args) {
@@ -13,6 +9,8 @@ public class student4 {
         ex2();
         ex3();
         ex4();
+
+
     }
 
     private static Integer addNumbers(ArrayList<Integer> numList) { // 1,2,3,4
@@ -26,49 +24,67 @@ public class student4 {
     private static void ex1() {
         int[] ints = {1, 2, 3, 4};
         var newList = new ArrayList<Integer>();
-        for (int i : ints) {
+        for (int i : ints) { //for each integer inside ints
             newList.add(i);
         }
         var result = addNumbers(newList);
         System.out.println("the sum of the Array is: " + result);
     }
 
-
-
+    private static float avgNumbers(ArrayList<Float> numList) { // 1,2,3,4
+        float retveal = 0;
+        float average = 0;
+        for (float i : numList) { // For each float "i" in this numList, I'm going to do the following...
+            retveal = retveal + i;// = is an assignment operator.
+            average = retveal / numList.size();
+        }
+        return average;
+    }
 
     private static void ex2() {
-        List<Integer> list = Arrays.asList(1, 2, 3, 4); // list of arrays
-        double sum = 0;
-        for (int i : list) { // for each int of the list i'm going to do the following
-            sum += i;
+        int[] ints = {1, 2, 3, 4};
+        var newList = new ArrayList<Float>();
+        for (float i : ints) {
+            newList.add(i);
         }
-        double average = sum / list.size();
 
-        System.out.println("the Average is = " + average);
+        var result = avgNumbers(newList);
+        System.out.println("the average of the Array is: " + result);
+
     }
+    private static void doubleNumber(ArrayList<Integer> numList){
+        var retveal = 0;
+        var Doublenum = new ArrayList<Integer>();
+        for (int i : numList){
+             retveal = i *2;
+             Doublenum.add(retveal);
+        }
+        System.out.println(Doublenum);
 
 
-
+    }
     private static void ex3() {
-        List<Integer> list = Arrays.asList(1,2,3,4); // List of Arrays
-
-        List<Integer> doubleArrayListNumbers = list.stream() // formule to double each number in the Array
-                .map(i -> i*2)
-                .collect(toList());
-
-        System.out.println("Double values of ArrayList Integers: "+doubleArrayListNumbers);
-
+        int[] ints = {1, 2, 3, 4};
+        var newList = new ArrayList<Integer>();
+        for (int i : ints) { //for each integer inside ints
+            newList.add(i);
+        }
+        doubleNumber(newList);
+    }
+    private static void ex4(){
+        int listMax = 5;
+        createList(listMax);
     }
 
-    private static void ex4() {
-        int maxNumber = 5;
+    private static void createList(Integer maxNumber){
+        var Createlist = new ArrayList<Integer>();
+        for(int i = 0; i<=maxNumber;i++){
+            Createlist.add(i);
 
-    }
+        }
+        System.out.println("Max number is: " + Createlist);
+}
 
-    private static void createlist(int maxNumber) {
+}
 
-
-    }
-
-    }
 
