a22527fba20e69addd60a1c538dce1dbfbc63f28
katie <kcohen015@gmail.com>
2022-04-28 12:32:42 -0400

    Exercise 6 update

--- src/main/java/com/xpanxion/assignments/groupf/student14.java
+++ src/main/java/com/xpanxion/assignments/groupf/student14.java
@@ -1,28 +1,28 @@
 package com.xpanxion.assignments.groupf;
 
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class student14 {
     public static void main(String[] args) {
 //        ex1();
-//        ex2();
-//        ex3();
-//        ex4();
-        ex5();
+ //       ex2();
+ //      ex3();
+ //      ex4();
+//        ex5();
+       ex6();
     }
 
     //vars, loops, ifs, functions
     private static void ex1() {
         int[] ints = {1, 2, 3}; //declare array
 
-        ArrayList<Integer> numList = new ArrayList<>();//declare new array list
+        var numList = new ArrayList<Integer>();//declare new array list
         //Implement function
         for (int i : ints) {
             numList.add(i);
-        }
+        } // map
+
         var sum = addNumbers(numList);
         System.out.println("The sum is " + sum);
     }
@@ -36,11 +36,10 @@ public class student14 {
         return sum;
     }
 
-
     private static void ex2() {
         ArrayList<Integer> numList = new ArrayList<>();//declare new array list
         //convert int to integer
-        int[] ints = {1, 2, 3, 4};
+        int[] ints = {11, 2, 3, 4, 5};
         //var newList = new ArrayList<Integer>();
         for (int i : ints) {
             numList.add(i);
@@ -56,16 +55,13 @@ public class student14 {
         Float retVal = 0F;
         var sum = addNumbers(numList);
         float length = numList.size();
-
         float average = sum / length;
-
         return average;
-
     }
 
 
     private static void ex3() {
-        ArrayList<Integer> numList = new ArrayList<>();//declare new array list
+        var numList = new ArrayList<Integer>();//declare new array list
         //convert int to integer
         int[] ints = {1, 2, 3, 4};
         //add the numbers to the numList
@@ -73,18 +69,14 @@ public class student14 {
             numList.add(i);
         }
         doubleNumbers(numList);
-
+        System.out.println(numList);//print list
     }
 
     private static void doubleNumbers(ArrayList<Integer> numList) {
-
         for (int i = 0; i < numList.size(); i++) {
-
             int timesTwo = numList.get(i);
             numList.set(i, (timesTwo * 2));
         }
-        System.out.println(numList);//print list
-
     }
 
     private static void ex4() {
@@ -96,7 +88,7 @@ public class student14 {
 //create list
 //for loop to determine the type of numbers in list
         ArrayList<Integer> numList = new ArrayList<>();
-        for (int i = 1; i <= maxNumber; ++i) {
+        for (int i = 1; i <= maxNumber; i++) {
             numList.add(i);
         }
         return numList;
@@ -116,10 +108,53 @@ public class student14 {
         int firstNumber = numList.get(0);
         int lastNumber = numList.get(numList.size() - 1);
         for (int i = firstNumber; i < lastNumber; i++) {
-            if (!numList.contains(i)) {
+            if (!numList.contains(i)) { // ! means "not" negation operator
                 missingNumbers.add(i);
             }
         }
         return missingNumbers;
     }
+
+//    private static void ex6() {
+//        int[] ints = {0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0};
+//        ArrayList<Integer> numList = new ArrayList<>();
+//       // int[] sorted = sortCollection;
+//
+//        for (int i = 0; i < ints.length; i++) {
+//            numList.add(ints [i]);
+//        }
+//        System.out.println((Arrays.toString(sortCollection(ints))));
+//    }
+//    private static int[] sortCollection (int[] ints) {
+//             Arrays.sort(ints);
+//        return ints;
+
+    private static void ex6() {
+        //ints array
+        int[] ints = {0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0};
+        //initializing string a
+        System.out.println(organizeZerosAndOnes(ints));
+    }
+     private static String organizeZerosAndOnes(int[] ints) {
+        String result = "";
+        //initializing left string
+        String leftZeros = "";
+        //initializing right string
+        String rightOnes = "";
+        //for loop because we are iterating through every element of ints array
+        for (int i = 0; i < ints.length; i++) {
+            if(ints[i] == 0) {
+                //we are calling the object integer to convert an array element to string element
+                leftZeros += Integer.toString(ints[i]);
+            }
+            else {
+                rightOnes += Integer.toString(ints[i]);
+            }
+        }
+        result = leftZeros + rightOnes;
+        return result;
+    }
+
 }
+
+
