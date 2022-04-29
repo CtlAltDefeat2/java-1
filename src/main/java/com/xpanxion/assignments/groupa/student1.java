1cc554d0e3f35d9de3fb12a1f95d385688d5d473
akbagga1 <103646402+akbagga1@users.noreply.github.com>
2022-04-28 20:57:19 -0400

    update ex6

--- src/main/java/com/xpanxion/assignments/groupa/student1.java
+++ src/main/java/com/xpanxion/assignments/groupa/student1.java
@@ -1,10 +1,6 @@
 package com.xpanxion.assignments.groupa;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 
 public class student1 {
@@ -14,18 +10,20 @@ public class student1 {
         //ex2();
         //ex3();
         //ex4();
-        ex5();
+        //ex5();
+        ex6();
     }
 
     private static void ex1() {
         int[] nums = {1, 2, 3};
         ArrayList<Integer> numList = new ArrayList<Integer>();
-        for (int i: nums) {
+        for (int i : nums) {
             numList.add(i);
         }
         var r = addNumbers(numList);
         System.out.println(r);
     }
+
     private static Integer addNumbers(ArrayList<Integer> numList) {
         Integer retVal = 0;
         for (Integer i : numList) {   // for each integer "i" in this numList
@@ -43,11 +41,12 @@ public class student1 {
         var r = avgNumbers(numList);
         System.out.println("Average:" + r);
     }
+
     private static Float avgNumbers(ArrayList<Integer> numList) {
         var sum = addNumbers(numList);
-    Float length= Float.valueOf(numList.size());
-    Float average = sum/length;
-    return average;
+        Float length = Float.valueOf(numList.size());
+        Float average = sum / length;
+        return average;
     }
 
     private static void ex3() {
@@ -59,50 +58,75 @@ public class student1 {
         var result = doubleArray(newList);
         System.out.println(result);
     }
+
     private static ArrayList<Integer> doubleArray(ArrayList<Integer> numList) {
         for (int i = 0; i < numList.size(); i++) {
             numList.set(i, numList.get(i) * 2);
         }
-            return numList;
-        }
+        return numList;
+    }
 
-        private static void ex4 () {
-            var list = createList(5);
-            System.out.println(list);
-        }
-        private static ArrayList<Integer> createList (Integer maxNumber){
+    private static void ex4() {
+        var list = createList(5);
+        System.out.println(list);
+    }
 
-            ArrayList<Integer> numList = new ArrayList<>();
-            for (int i = 1; i <= maxNumber; ++i) {
-                numList.add(i);
-            }
-            return numList;
-        }
+    private static ArrayList<Integer> createList(Integer maxNumber) {
 
-        private static void ex5 () {
-            int[] ints = {1, 2, 3, 5, 6, 9, 11};
-            ArrayList<Integer> numList = new ArrayList<>();
-            for (int i = 0; i < ints.length; i++) {
-                numList.add(ints[i]);
-            }
-            System.out.println(findMissingNumbers(numList));
+        ArrayList<Integer> numList = new ArrayList<>();
+        for (int i = 1; i <= maxNumber; ++i) {
+            numList.add(i);
         }
+        return numList;
+    }
+
+    private static void ex5() {
+        int[] ints = {1, 2, 3, 5, 6, 9, 11};
+        ArrayList<Integer> numList = new ArrayList<>();
+        for (int i = 0; i < ints.length; i++) {
+            numList.add(ints[i]);
+        }
+        System.out.println(findMissingNumbers(numList));
+    }
 
     private static ArrayList<Integer> findMissingNumbers(ArrayList<Integer> numList) {
         var missingNumbers = new ArrayList<Integer>();
         int firstNumber = numList.get(0);
-        int lastNumber = numList.get(numList.size()-1);
-        for (int i= firstNumber; i < lastNumber; i++) {
+        int lastNumber = numList.get(numList.size() - 1);
+        for (int i = firstNumber; i < lastNumber; i++) {
             if (!numList.contains(i)) {
-                    missingNumbers.add(i);
+                missingNumbers.add(i);
             }
         }
         return missingNumbers;
     }
+
+    private static void ex6() {
+        //ints array
+        int[] ints = {0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0};
+    System.out.println(organizeZerosAndOnes(ints));
+    }
+    private static String organizeZerosAndOnes(int [] ints) {
+        //initialing string result
+        String result = "";
+        //initializing left string
+        String leftZeros = "";
+        //initializing right string
+        String rightOnes = "";
+        //for loop because we are iterating through every element of the int array
+        for (int i = 0; i < ints.length; i++) {
+            if (ints[i] == 0) {
+                //we are calling the object integer to convert an array element to string element
+                leftZeros += Integer.toString(ints[i]);
+            }
+            else {
+                rightOnes += Integer.toString(ints[i]);
+            }
+        }
+        result = leftZeros + rightOnes;
+        return result;
+    }
 }
-
-
-
 
 
      /*var list = new ArrayList<Integer>();
@@ -124,3 +148,5 @@ public class student1 {
 //            sum = sum + value;   //0+1=1, 1+2=3, 3+3=6
 //        }
 //        System.out.println("Sum of Array:" + sum);
+
+
